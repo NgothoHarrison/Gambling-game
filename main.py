7faad2c0801288cc6e90e0879c6572be44aedd3f
Hank <hank@vbox.Hank>
2025-01-31 15:13:08 +0300

    Betting program now starts

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("Hello world")
+def deposit():
+    while True:
+        my_amount = input("How much deposit do you want : $ ")
+
+        if my_amount.isdigit():
+            my_amount = int(my_amount)
+            if my_amount > 0 :
+                break
+            else:
+                print("Amount must be greater than o")
+        else:
+            print("Please enter a number.")
+
+    return my_amount
+        
+
+deposit()
