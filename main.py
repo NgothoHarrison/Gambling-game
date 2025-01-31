c55b6b8472aed1aa7bd383356798d17d90db4512
Hank <hank@vbox.Hank>
2025-01-31 16:22:19 +0300

    Set the valuables

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+MAX_LINES = 3
+MAX_BET = 1000
+MIN_BET = 1
+
+
 def deposit():
     while True:
         my_amount = input("How much deposit do you want : $ ")
@@ -12,6 +17,26 @@ def deposit():
             print("Please enter a number.")
 
     return my_amount
-        
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on(1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines .")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+
+        
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
