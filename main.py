b4934fb4fc694eb124089cbee8f493e83f62cf9f
Hank <hank@vbox.Hank>
2025-02-01 15:15:06 +0300

    Advance to validate input

--- main.py
+++ main.py
@@ -50,8 +50,14 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"Sorry, you only have ${balance} left. You can't bet ${total_bet}")
+        else:
+            break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is : $ {total_bet}")
     print(balance, lines, bet)
